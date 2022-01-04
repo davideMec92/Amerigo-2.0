21b4728ea0b860e4915ffd51415ee2d78b25a916
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-01-04 19:41:51 +0100

    Add CommunicationMessageBlock

--- project/hashgraph/models/communication/CommunicationMessageBlock.py
+++ project/hashgraph/models/communication/CommunicationMessageBlock.py
@@ -1,8 +1,10 @@
 from project.hashgraph.enums.CommunicationMessageTypes import CommunicationMessageTypes
+from project.hashgraph.models.Block import Block
 from project.hashgraph.models.communication.CommunicationMessage import CommunicationMessage
 
 
-class CommunicationMessageACK(CommunicationMessage):
-    def __init__(self):
+class CommunicationMessageBlock(CommunicationMessage):
+    def __init__(self, block: Block):
         super().__init__()
-        self.type = CommunicationMessageTypes.ACK
+        self.type = CommunicationMessageTypes.BLOCK
+        self.block = block
