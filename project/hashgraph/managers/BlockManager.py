2ef0705519d82d1a1e3ce050fb9bceaf1848d257
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-01-04 19:39:43 +0100

    Add BlockManager logic

--- project/hashgraph/managers/BlockManager.py
+++ project/hashgraph/managers/BlockManager.py
@@ -1,12 +1,42 @@
-from project.hashgraph.helpers.BaseFifoQueue import BaseFifoQueue
+from threading import Thread
+
+from project.Logger.Logger import Logger, LogLevels
+from project.bluetooth_client import BluetoothClient
+from project.bluetooth_settings import BluetoothSettings
+from project.hashgraph.helpers.FifoQueue import FifoQueue  # type: ignore
 from project.hashgraph.models.Block import Block
+from project.hashgraph.models.communication.CommunicationMessageBlock import CommunicationMessageBlock
 
 
-class BlockManager:
-    __instance = None
+class BlockManager(Thread):
+    toCommitBlocks: FifoQueue[Block] = []
     isSendEnabled: bool = True
 
+    def stopSending(self) -> None:
+        self.isSendEnabled = False
 
-    def __init__(self):
-        self.toCommitBlocks = BaseFifoQueue[Block]()
+    def sendBlock(self, block: Block) -> None:
+        self.toCommitBlocks.push(block)
+
+    def run(self) -> None:
+        isBlockSent: bool = False
+
+        while self.isSendEnabled is True:
+            if self.toCommitBlocks.isEmpty() is False:
+                isBlockSent = True
+                try:
+                    bluetoothClient: BluetoothClient = BluetoothClient(BluetoothSettings.BluetoothServerBluetoothMAC, BluetoothSettings.BluetoothServerUUID)
+                    bluetoothClient.sendMessage(CommunicationMessageBlock(self.toCommitBlocks.peek().build()))
+                except Exception as e:
+                    Logger.createLog(LogLevels.ERROR, __file__, 'Error during building and sending block: ' + str(e))
+                    isBlockSent = False
+                finally:
+                    if isBlockSent is True:
+                        self.toCommitBlocks.peek().store.removeRoundsBeforeRoundCreatedIndex(self.toCommitBlocks.peek().roundCreated)
+                        self.toCommitBlocks.remove()
+
+
+
+
+
 
