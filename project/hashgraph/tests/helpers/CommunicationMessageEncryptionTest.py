c12850ed69719c7343369197da72b32dd4a3d901
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-01-04 17:49:22 +0100

    Add Encryption/Decryption unit tests

--- project/hashgraph/tests/helpers/CommunicationMessageEncryptionTest.py
+++ project/hashgraph/tests/helpers/CommunicationMessageEncryptionTest.py
@@ -1,14 +1,16 @@
 import unittest
 
+from project.hashgraph.helpers.CommunicationMessageDecrypter import CommunicationMessageDecrypter
 from project.hashgraph.helpers.CommunicationMessageEncrypter import CommunicationMessageEncrypter
 from project.hashgraph.models.communication.CommunicationMessageACK import CommunicationMessageACK
 
 
-class CommunicationMessageEncrypterTest(unittest.TestCase):
-    def testCommunicationMessageEncryption(self):
+class CommunicationMessageEncryptionTest(unittest.TestCase):
+    def testCommunicationMessageEncryptionDecryptionOK(self):
         communicationMessageACK = CommunicationMessageACK()
-        print('ENCRYPT: ' + CommunicationMessageEncrypter.encrypt(communicationMessageACK))
-        self.assertTrue(True)  # add assertion here
+        encryptedMessage = CommunicationMessageEncrypter.encrypt(communicationMessageACK)
+        decryptedMessage = CommunicationMessageDecrypter.decrypt(encryptedMessage)
+        self.assertTrue(communicationMessageACK.toJson() == decryptedMessage)
 
 
 if __name__ == '__main__':
--- project/hashgraph/tests/helpers/FifoQueueTest.py
+++ project/hashgraph/tests/helpers/FifoQueueTest.py
@@ -1,11 +1,10 @@
 import unittest
-from typing import List
 
-from project.hashgraph.helpers.BaseFifoQueue import BaseFifoQueue
+from project.hashgraph.helpers.FifoQueue import FifoQueue
 from project.hashgraph.models.Peer import Peer
 
 
-class BaseFifoQueueTest(unittest.TestCase):
+class FifoQueueTest(unittest.TestCase):
     peer1 = Peer()
     peer1.deviceId = 'Peer 1'
     peer2 = Peer()
@@ -14,36 +13,36 @@ class BaseFifoQueueTest(unittest.TestCase):
     peer3.deviceId = 'Peer 3'
 
     def testIsEmptyOK(self):
-        baseFifoQueue = BaseFifoQueue[Peer]()
+        baseFifoQueue = FifoQueue[Peer]()
         self.assertTrue(baseFifoQueue.size() == 0 and baseFifoQueue.isEmpty() is True)
 
     def testPutItemOK(self):
-        baseFifoQueue = BaseFifoQueue[Peer]()
-        baseFifoQueue.put(self.peer1)
+        baseFifoQueue = FifoQueue[Peer]()
+        baseFifoQueue.push(self.peer1)
         self.assertTrue(baseFifoQueue.size() == 1 and baseFifoQueue.isEmpty() is False)
 
     def testGetItemOK(self):
-        baseFifoQueue = BaseFifoQueue[Peer]()
-        baseFifoQueue.put(self.peer1)
-        baseFifoQueue.get()
-        self.assertTrue(baseFifoQueue.size() == 0 and baseFifoQueue.isEmpty() is True)
+        fifoQueue = FifoQueue[Peer]()
+        fifoQueue.push(self.peer1)
+        fifoQueue.pop()
+        self.assertTrue(fifoQueue.size() == 0 and fifoQueue.isEmpty() is True)
 
     def testPeekItemOK(self):
-        baseFifoQueue = BaseFifoQueue[Peer]()
-        baseFifoQueue.put(self.peer1)
+        baseFifoQueue = FifoQueue[Peer]()
+        baseFifoQueue.push(self.peer1)
         baseFifoQueue.peek()
         self.assertTrue(baseFifoQueue.size() == 1 and baseFifoQueue.isEmpty() is False)
 
     def testFifoOK(self):
-        baseFifoQueue = BaseFifoQueue[Peer]()
-        baseFifoQueue.put(self.peer1)
-        baseFifoQueue.put(self.peer2)
-        baseFifoQueue.put(self.peer3)
+        baseFifoQueue = FifoQueue[Peer]()
+        baseFifoQueue.push(self.peer1)
+        baseFifoQueue.push(self.peer2)
+        baseFifoQueue.push(self.peer3)
         baseFifoQueue.peek()
         self.assertTrue(baseFifoQueue.peek().deviceId == 'Peer 1')
-        baseFifoQueue.get()
+        baseFifoQueue.pop()
         self.assertTrue(baseFifoQueue.peek().deviceId == 'Peer 2')
-        baseFifoQueue.get()
+        baseFifoQueue.pop()
         self.assertTrue(baseFifoQueue.peek().deviceId == 'Peer 3')
 
 
