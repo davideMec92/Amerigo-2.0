7adc2e009347d67f5c3c22dbf26ab2415407255b
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-01-04 18:46:01 +0100

    Add LockInitHelper class

---
+++ project/hashgraph/helpers/LockInitHelper.py
@@ -0,0 +1,17 @@
+from project.hashgraph.models.Hashgraph import Hashgraph
+
+
+class LockInitHelper:
+    @staticmethod
+    def initHashgraphGossipLocks(hashgraph: Hashgraph):
+        count: int = 1
+        for deviceId, peer in hashgraph.peers:
+            if hashgraph.myPeer.deviceId == deviceId:
+                if count % 2 == 0:
+                    hashgraph.isHashgraphReceiveLocked = True
+                    hashgraph.isHashgraphGossipLocked = False
+                else:
+                    hashgraph.isHashgraphGossipLocked = True
+                    hashgraph.isHashgraphReceiveLocked = False
+            count = count + 1
+            
