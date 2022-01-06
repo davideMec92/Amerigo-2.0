64b3ca65028143288e041be57a1bb00df912043a
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-01-06 20:16:06 +0100

    Update PeerStatus enum

--- project/hashgraph/enums/PeerStatus.py
+++ project/hashgraph/enums/PeerStatus.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class PeerStatus(Enum):
-    CONNECTED = 0
-    WARNING = 1
-    DISCONNECTED = 2
+class PeerStatus(str, Enum):
+    CONNECTED = 'CONNECTED'
+    WARNING = 'WARNING'
+    DISCONNECTED = 'DISCONNECTED'
