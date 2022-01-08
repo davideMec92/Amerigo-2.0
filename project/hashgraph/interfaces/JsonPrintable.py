cb1ca6e9f579a5bae0e33696a0076bef9bac7523
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-01-08 14:40:03 +0100

    Add filter function to JsonPrintable methods

--- project/hashgraph/interfaces/JsonPrintable.py
+++ project/hashgraph/interfaces/JsonPrintable.py
@@ -1,8 +1,17 @@
 import json
 
+
 class JsonPrintable():
     def toJson(self) -> str:
-        return json.dumps(self, default=lambda o: dict((key, value) for key, value in o.__dict__.items() if value), allow_nan=False)
+        return json.dumps(self, default=lambda o: dict(
+            (key, value) for key, value in o.__dict__.items() if self.filter(value)), allow_nan=False)
 
     def toPrettyJson(self) -> str:
-        return json.dumps(self, default=lambda o: dict((key, value) for key, value in o.__dict__.items() if value), allow_nan=False, sort_keys=True, indent=4)
+        return json.dumps(self, default=lambda o: dict((key, value) for key, value in o.__dict__.items() if self.filter(value)),
+                          allow_nan=False, sort_keys=True, indent=4)
+
+    def filter(self, value) -> bool:
+        if value is None:
+            return False
+
+        return True
