1956c91aadacc3e51e41916c4af9ac4798857583
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-03-17 01:07:46 +0100

    Integrate BaseSocketServerConnection logic

--- project/hashgraph/models/communication/BaseSocketServerConnection.py
+++ project/hashgraph/models/communication/BaseSocketServerConnection.py
@@ -1,10 +1,43 @@
+from socket import socket
 from threading import Thread
-from dotenv import load_dotenv
+from typing import Dict, List
 
-class BaseSocketServerConnection(Thread):
+from project.Logger.Logger import Logger, LogLevels
+from project.hashgraph.interfaces.Callbacks.CommunicationCallback import CommunicationCallback
+from project.hashgraph.interfaces.Callbacks.ServerConnectionRemoveCallback import ServerConnectionRemoveCallback
+from project.hashgraph.models.communication.BaseSocketConnection import BaseSocketConnection
 
-    def __int__(self):
+
+class BaseSocketServerConnection(Thread, ServerConnectionRemoveCallback):
+
+    def __int__(self, connectionCallbacks: List[CommunicationCallback]):
         self.isRunning = False
-        self.connectionCallbacks:
-
+        self.connectionCallbacks: List[CommunicationCallback] = connectionCallbacks
+        self.incomingConnections: Dict[str, BaseSocketConnection] = {}
+        self.serverSocket: socket = None
+        self.port: int = None
         Thread.__init__(self)
+
+    def addConnectionCallback(self, connectionCallback: CommunicationCallback) -> None:
+        self.connectionCallbacks.append(connectionCallback)
+
+    def stopServer(self) -> None:
+        Logger.createLog(LogLevels.DEBUG, __file__, 'Closing all incoming connections..')
+
+        for socketConnectionId, socketConnection in self.incomingConnections.items():
+            socketConnection.closeSocket()
+
+        self.incomingConnections = {}
+        self.serverSocket.close()
+        self.isRunning = False
+
+    def run(self):
+        raise Exception("Implement server socket logic")
+
+    def removeSocketConnection(self, connectionKey: str) -> None:
+        super().removeSocketConnection(connectionKey)
+        if connectionKey in self.incomingConnections:
+            self.incomingConnections.pop(connectionKey)
+            Logger.createLog(LogLevels.DEBUG, __file__, "Removed connection with key: " + str(connectionKey))
+
+
