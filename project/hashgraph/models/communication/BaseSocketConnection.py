a6a89ade3edaa272a8290558da7050b9bd5fd4ab
Davide Maccarrone <davide.nunzio.maccarrone@gmail.com>
2022-03-17 01:07:18 +0100

    Fix BaseSocketConnection class method for incoming messages

--- project/hashgraph/models/communication/BaseSocketConnection.py
+++ project/hashgraph/models/communication/BaseSocketConnection.py
@@ -1,3 +1,4 @@
+import time
 from socket import socket
 from threading import Thread
 
@@ -7,7 +8,6 @@ from project.hashgraph.enums.CommunicationMessageTypes import CommunicationMessa
 from project.hashgraph.helpers.CommunicationMessageDecrypter import CommunicationMessageDecrypter
 from project.hashgraph.helpers.FifoQueue import FifoQueue
 from project.hashgraph.helpers.Hash import Hash
-from project.hashgraph.helpers.SocketHelper import SocketHelper
 from project.hashgraph.interfaces.Callbacks.CommunicationCallback import CommunicationCallback
 from project.hashgraph.interfaces.Callbacks.ServerConnectionRemoveCallback import ServerConnectionRemoveCallback
 from project.hashgraph.models.communication.CommunicationMessage import CommunicationMessage
@@ -64,7 +64,8 @@ class BaseSocketConnection(Thread):
                         encryptedMessage = str(encryptedMessage)
                         encryptedMessage = encryptedMessage.lstrip('b')
                         encryptedMessage = encryptedMessage.replace("'", "")
-                    self.socket.send(str(encryptedMessage) + "\r\n")
+                    # self.socket.send(str(encryptedMessage) + "\r\n")
+                    self.socket.send(str(encryptedMessage))
                     Logger.createLog(LogLevels.DEBUG, __file__ , "Sent encrypted: " + str(communicationMessage.type) + 'to: ' + str(self.address))
 
                     # If block sent, close socket
@@ -95,15 +96,12 @@ class BaseSocketConnection(Thread):
         # Listening for incoming messages while mRun is True
         while self.isMessageSenderEnabled is True:
             try:
-                # server waits for incoming connections on accept()
-                # for incoming requests, new socket created on return
-                connectionSocket, addr = self.socket.accept()
 
                 # receive sentence on newly established connectionSocket
-                incomingMessage = self.recv_timeout(connectionSocket)
+                incomingMessage = self.recv_timeout()
 
-                if incomingMessage is None:
-                    Logger.createLog(LogLevels.DEBUG, __file__, 'Receviced None message!')
+                if incomingMessage is None or incomingMessage == '':
+                    Logger.createLog(LogLevels.INFO, __file__, 'Received None message!')
                     continue
 
                 Logger.createLog(LogLevels.DEBUG, __file__, "New Message: " + str(incomingMessage))
@@ -140,6 +138,46 @@ class BaseSocketConnection(Thread):
             except Exception as e:
                 Logger.createLog(LogLevels.ERROR, __file__, "Error while receiving data from socket: " + str(e))
                 pass
+
+    def parseReceivedString(self, receivedString):
+        receivedString = receivedString.replace("\\n", "")
+        receivedString = receivedString.lstrip('b')
+        return receivedString
             
+    def recv_timeout(self, timeout=2):
+        # make socket non blocking
+        self.socket.setblocking(0)
+
+        # total data partwise in an array
+        total_data = [];
+        data = '';
+
+        # beginning time
+        begin = time.time()
+        while 1:
+            # if you got some data, then break after timeout
+            if total_data and time.time() - begin > timeout:
+                break
+
+            # if you got no data at all, wait a little longer, twice the timeout
+            elif time.time() - begin > timeout * 2:
+                break
+
+            # recv something
+            try:
+                data = self.socket.recv(8192)
+                if data:
+                    total_data.append(self.parseReceivedString(str(data)))
+                    # change the beginning time for measurement
+                    begin = time.time()
+                else:
+                    # sleep for sometime to indicate a gap
+                    time.sleep(0.1)
+            except:
+                pass
+
+        # join all parts to make final string
+        return ''.join(total_data)
+
     def sanitizeMessage(self, message: str) -> str:
         return message.replace("\\n", "").lstrip('b')
